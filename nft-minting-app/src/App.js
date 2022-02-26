90b092acf92f56e3d7f1396ac6a8ee4435696a12
Nam Vu <vuhoangnamkstn97@gmail.com>
2022-02-26 16:47:15 +0700

    update opensea link

--- nft-minting-app/src/App.js
+++ nft-minting-app/src/App.js
@@ -201,9 +201,9 @@ function App() {
         style={{ padding: 24, backgroundColor: "var(--primary)" }}
         image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
-        <a href={CONFIG.MARKETPLACE_LINK}>
+        {/* <a href={CONFIG.MARKETPLACE_LINK}>
           <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
-        </a>
+        </a> */}
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
